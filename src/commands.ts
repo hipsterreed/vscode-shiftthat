df6b3026e904d5c759dc13978c426899aff54191
Jimmy Arroyo <jamesarroyo5@gmail.com>
2019-04-09 22:45:17 -0600

    Added ability to sort workspace and user settings alphabetically.

---
+++ src/commands.ts
@@ -0,0 +1,47 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+
+export async function shiftUserSettings(): Promise<void> { 
+	await vscode.commands.executeCommand('workbench.action.openGlobalSettings');
+	shiftSettings();
+}
+
+export async function shiftWorkspaceSettings(): Promise<void> { 
+	await vscode.commands.executeCommand('workbench.action.openWorkspaceSettings');
+	shiftSettings();
+}
+
+async function shiftSettings(): Promise<void> {
+	await vscode.commands.executeCommand('settings.switchToJSON');
+
+	const activeEditor: any = vscode.window.activeTextEditor;
+	const settingsUri: string = activeEditor.document.uri.fsPath;
+
+	var settings = await getSettingsObj(settingsUri);
+	var sortedSettings = sortObjKeysAlphabetically(settings);
+
+	saveSettings(settingsUri, sortedSettings);
+}
+
+async function getSettingsObj(uri: string): Promise<Object> {
+	const settingsDocument = await vscode.workspace.openTextDocument(uri);
+	const text = settingsDocument.getText();
+
+	return JSON.parse(text);
+}
+
+function sortObjKeysAlphabetically(obj: any): Object {
+	var ordered: any = {};
+	Object.keys(obj).sort().forEach((key) => {
+		ordered[key] = obj[key];
+	});
+	return ordered;
+}
+
+function saveSettings(settingsUri: string, settings: Object): void {
+	fs.writeFile(settingsUri, JSON.stringify(settings, null, 2), (err: Error) => {
+		if(err) {
+		  console.error(err);
+		}
+	}); 
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,13 @@
 import * as vscode from 'vscode';
+import { shiftUserSettings, shiftWorkspaceSettings } from './commands';
 
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "shiftthat" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let shiftThatUserDisposable = vscode.commands.registerCommand('extension.shiftThatUser', () => {
-		// The code you place here will be executed every time your command is executed
+	const shiftUserDisp = vscode.commands.registerCommand('extension.shiftThatUser', shiftUserSettings);
+    const shiftWorkspaceDisp = vscode.commands.registerCommand('extension.shiftThatWorkspace', shiftWorkspaceSettings);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Shifted User Settings!!!');
-	});
-
-	let shiftThatWorkspaceDisposable = vscode.commands.registerCommand('extension.shiftThatWorkspace', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Shifted Workspace Settings!!!');
-	});
-
-	context.subscriptions.push(shiftThatUserDisposable);
-	context.subscriptions.push(shiftThatWorkspaceDisposable);
-
+	context.subscriptions.push(shiftUserDisp, shiftWorkspaceDisp);
 }
 
 // this method is called when your extension is deactivated
